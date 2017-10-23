ad4b04a3e3e309388485ae0daf1bb2532e440182
Leonardo Alves <leonardoalves1011@hotmail.com>
2017-10-23 15:43:38 -0200

    Tarde: Criando as modelos (classes das tabelas)

---
+++ WebApplication1/WebApplication1/Models/Item.cs
@@ -0,0 +1,22 @@
+﻿namespace WebApplication1.Models
+{
+    public class Item
+    {
+        public int ItemID { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+
+        public int Raridade { get; set; }
+
+        public int DanoMaximo { get; set; }
+
+        //Relacionamento item -- TipoItem
+
+        public int TipoItemID { get; set; }
+
+        public virtual TipoItem _TipoItem { get; set; }
+
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/TipoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Models
+{
+    public class TipoItem
+    {
+        public int TipoItemID { get; set; }
+
+        public string Nome { get; set; }
+    }
+}
