0852f55a052be6185893769e64f56bf4c6f102f5
Leonardo Alves <leonardoalves1011@hotmail.com>
2017-10-23 15:34:24 -0200

    Tarde: criando o contexto

---
+++ WebApplication1/WebApplication1/Models/Contexto/Meu contexto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models.Contexto
+{
+    public class Meu_contexto : DbContext
+    {
+        public Meu_contexto() : base("strConn")
+        {
+
+        }
+    }
+}
